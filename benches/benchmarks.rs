8a6aa372b62f9f1a4dfdfafef5e72d6913d99e7d
Jakub Žádník <kubouch@gmail.com>
2024-10-15 21:58:32 +0300

    Start benchmarking with tango (#15)
    Sets up some benchmarks using
    [tango-bench](https://github.com/bazhenov/tango).
    
    Apart from total compile time, each stage is also benchmarked
    separately.
    
    Result on my machine (AMD Ryzen 9 5950X)
    ```
    > target/benchmarks/compiler/benchmarks solo -s 100 --warmup true
    def_parse                                           [   3.8 us ...   3.8 us ...   3.9 us ]  stddev:  22.7 ns
    def_resolve                                         [ 854.0 ns ... 863.0 ns ... 880.5 ns ]  stddev:   5.6 ns
    def_typecheck                                       [ 686.6 ns ... 725.7 ns ... 753.4 ns ]  stddev:  13.5 ns
    def_resolve_merge                                   [   1.0 us ...   1.0 us ...   1.1 us ]  stddev:   7.1 ns
    def_typecheck_merge                                 [ 692.3 ns ... 765.9 ns ... 789.9 ns ]  stddev:  15.0 ns
    def_compile                                         [   4.9 us ...   5.0 us ...   5.0 us ]  stddev:  19.0 ns
    def_nu_old                                          [   3.2 us ...   3.3 us ...   3.4 us ]  stddev:  29.6 ns
    if_parse                                            [   4.8 us ...   4.8 us ...   4.9 us ]  stddev:  28.0 ns
    if_resolve                                          [ 445.6 ns ... 450.4 ns ... 458.1 ns ]  stddev:   2.5 ns
    if_typecheck                                        [ 402.8 ns ... 408.8 ns ... 416.7 ns ]  stddev:   3.8 ns
    if_resolve_merge                                    [ 567.8 ns ... 575.0 ns ... 605.2 ns ]  stddev:   5.4 ns
    if_typecheck_merge                                  [ 426.1 ns ... 433.1 ns ... 442.9 ns ]  stddev:   3.9 ns
    if_compile                                          [   5.5 us ...   5.6 us ...   5.7 us ]  stddev:  28.5 ns
    if_nu_old                                           [   4.8 us ...   4.9 us ...   5.0 us ]  stddev:  40.5 ns
    nu_old_empty                                        [ 317.7 ns ... 325.0 ns ... 333.8 ns ]  stddev:   3.0 ns
    ```

---
+++ benches/benchmarks.rs
@@ -0,0 +1,250 @@
+use std::process::exit;
+
+use nu_protocol::engine::{EngineState, StateWorkingSet};
+use tango_bench::{benchmark_fn, tango_benchmarks, tango_main, Benchmark, IntoBenchmarks};
+
+use new_nu_parser::compiler::Compiler;
+use new_nu_parser::parser::Parser;
+use new_nu_parser::resolver::Resolver;
+use new_nu_parser::typechecker::Typechecker;
+
+/// Files in benches/nu/ we want to benchmark (without .nu suffix)
+const BENCHMARKS: &[&str] = &["def", "if"];
+
+enum Stage {
+    Parse,
+    Resolve,
+    Typecheck,
+    ResolveMerge,
+    TypecheckMerge,
+    Compile,
+    Nu,
+}
+
+/// Stages of compilation we want to profile
+const STAGES: &[Stage] = &[
+    Stage::Parse,
+    Stage::Resolve,
+    Stage::Typecheck,
+    Stage::ResolveMerge,
+    Stage::TypecheckMerge,
+    Stage::Compile,
+    Stage::Nu,
+];
+
+/// Set up compiler with selected stages pre-run
+fn setup_compiler(
+    fname: &str,
+    do_parse: bool,
+    do_resolve: bool,
+    do_typecheck: bool,
+) -> Result<(Compiler, usize), String> {
+    let mut compiler = Compiler::new();
+    let span_offset = compiler.span_offset();
+
+    let contents = std::fs::read(fname).map_err(|_| format!("Cannot find file {fname}"))?;
+    compiler.add_file(&fname, &contents);
+
+    if do_parse {
+        let parser = Parser::new(compiler, span_offset);
+        compiler = parser.parse();
+
+        if !compiler.errors.is_empty() {
+            return Err(format!("Error parsing file {fname}"));
+        }
+    }
+
+    if do_resolve {
+        let mut resolver = Resolver::new(&compiler);
+        resolver.resolve();
+
+        if !compiler.errors.is_empty() {
+            return Err(format!("Error resolving file {fname}"));
+        }
+
+        compiler.merge_name_bindings(resolver.to_name_bindings());
+    }
+
+    if do_typecheck {
+        let mut typechecker = Typechecker::new(&compiler);
+        typechecker.typecheck();
+
+        if !compiler.errors.is_empty() {
+            return Err(format!("Error typechecking file {fname}"));
+        }
+
+        compiler.merge_types(typechecker.to_types());
+    }
+
+    Ok((compiler, span_offset))
+}
+
+/// Parse only
+pub fn parse(mut compiler: Compiler, span_offset: usize) {
+    let parser = Parser::new(compiler, span_offset);
+    compiler = parser.parse();
+
+    if !compiler.errors.is_empty() {
+        eprintln!("Error resolving def.");
+        exit(1);
+    }
+}
+
+/// Resolve only
+pub fn resolve(mut compiler: Compiler, do_merge: bool) {
+    let mut resolver = Resolver::new(&compiler);
+    resolver.resolve();
+
+    if !compiler.errors.is_empty() {
+        eprintln!("Error resolving def.");
+        exit(1);
+    }
+
+    if do_merge {
+        compiler.merge_name_bindings(resolver.to_name_bindings());
+    }
+}
+
+/// Typecheck only
+pub fn typecheck(mut compiler: Compiler, do_merge: bool) {
+    let mut typechecker = Typechecker::new(&compiler);
+    typechecker.typecheck();
+
+    if !compiler.errors.is_empty() {
+        eprintln!("Error typechecking def.");
+        exit(1);
+    }
+
+    if do_merge {
+        compiler.merge_types(typechecker.to_types());
+    }
+}
+
+/// Run all compiler stages
+pub fn compile(mut compiler: Compiler, span_offset: usize) {
+    let parser = Parser::new(compiler, span_offset);
+    compiler = parser.parse();
+
+    if !compiler.errors.is_empty() {
+        eprintln!("Error resolving def.");
+        exit(1);
+    }
+
+    let mut resolver = Resolver::new(&compiler);
+    resolver.resolve();
+
+    if !compiler.errors.is_empty() {
+        eprintln!("Error resolving def.");
+        exit(1);
+    }
+
+    compiler.merge_name_bindings(resolver.to_name_bindings());
+
+    let mut typechecker = Typechecker::new(&compiler);
+    typechecker.typecheck();
+
+    if !compiler.errors.is_empty() {
+        eprintln!("Error typechecking def.");
+        exit(1);
+    }
+
+    compiler.merge_types(typechecker.to_types());
+}
+
+fn parse_nu_old(engine_state: &EngineState, contents: &[u8]) {
+    let mut working_set = StateWorkingSet::new(engine_state);
+    let _ = nu_parser::parse(&mut working_set, None, contents, false);
+}
+
+fn parse_nu_old_empty(engine_state: &EngineState) {
+    let mut working_set = StateWorkingSet::new(engine_state);
+    let _ = nu_parser::parse(&mut working_set, None, b"", false);
+}
+
+fn compiler_benchmarks() -> impl IntoBenchmarks {
+    let mut benchmarks: Vec<Benchmark> = vec![];
+
+    for bench_name in BENCHMARKS {
+        for stage in STAGES {
+            let bench_file = format!("benches/nu/{bench_name}.nu");
+
+            let bench = match stage {
+                Stage::Parse => {
+                    let name = format!("{bench_name}_parse");
+                    benchmark_fn(name, move |b| {
+                        let (compiler_def_init, span_offset) =
+                            setup_compiler(&bench_file, false, false, false)
+                                .expect("Error setting up compiler");
+                        b.iter(move || parse(compiler_def_init.clone(), span_offset))
+                    })
+                }
+                Stage::Resolve => {
+                    let name = format!("{bench_name}_resolve");
+                    benchmark_fn(name, move |b| {
+                        let (compiler_def_parsed, _) =
+                            setup_compiler(&bench_file.clone(), true, false, false)
+                                .expect("Error setting up compiler");
+                        b.iter(move || resolve(compiler_def_parsed.clone(), false))
+                    })
+                }
+                Stage::ResolveMerge => {
+                    let name = format!("{bench_name}_resolve_merge");
+                    benchmark_fn(name, move |b| {
+                        let (compiler_def_parsed, _) =
+                            setup_compiler(&bench_file.clone(), true, false, false)
+                                .expect("Error setting up compiler");
+                        b.iter(move || resolve(compiler_def_parsed.clone(), true))
+                    })
+                }
+                Stage::Typecheck => {
+                    let name = format!("{bench_name}_typecheck");
+                    benchmark_fn(name, move |b| {
+                        let (compiler_def_parsed, _) =
+                            setup_compiler(&bench_file.clone(), true, true, false)
+                                .expect("Error setting up compiler");
+                        b.iter(move || typecheck(compiler_def_parsed.clone(), false))
+                    })
+                }
+                Stage::TypecheckMerge => {
+                    let name = format!("{bench_name}_typecheck_merge");
+                    benchmark_fn(name, move |b| {
+                        let (compiler_def_parsed, _) =
+                            setup_compiler(&bench_file.clone(), true, true, false)
+                                .expect("Error setting up compiler");
+                        b.iter(move || typecheck(compiler_def_parsed.clone(), true))
+                    })
+                }
+                Stage::Compile => {
+                    let name = format!("{bench_name}_compile");
+                    benchmark_fn(name, move |b| {
+                        let (compiler_def_init, span_offset) =
+                            setup_compiler(&bench_file.clone(), false, false, false)
+                                .expect("Error setting up compiler");
+                        b.iter(move || compile(compiler_def_init.clone(), span_offset))
+                    })
+                }
+                Stage::Nu => {
+                    let name = format!("{bench_name}_nu_old");
+                    benchmark_fn(name, move |b| {
+                        let engine_state = EngineState::new();
+                        let contents = std::fs::read(bench_file.clone())
+                            .expect(&format!("Cannot find file {bench_file}"));
+                        b.iter(move || parse_nu_old(&engine_state, &contents))
+                    })
+                }
+            };
+
+            benchmarks.push(bench);
+        }
+    }
+
+    benchmarks.push(benchmark_fn(format!("nu_old_empty"), move |b| {
+        let engine_state = EngineState::new();
+        b.iter(move || parse_nu_old_empty(&engine_state))
+    }));
+
+    benchmarks
+}
+
+tango_benchmarks!(compiler_benchmarks());
+tango_main!();
